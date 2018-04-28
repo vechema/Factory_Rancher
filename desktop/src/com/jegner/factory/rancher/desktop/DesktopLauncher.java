6214fe5ed5061964990ecdc11e6d0ba9c46f3bc5
Jo Egner <vechema@gmail.com>
2018-04-28 15:15:42 -0500

    Create game from libgdx project generator

---
+++ desktop/src/com/jegner/factory/rancher/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jegner.factory.rancher.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jegner.factory.rancher.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
