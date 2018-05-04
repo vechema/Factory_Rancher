cc167d0652d4b79c17ff475a76fe405ec32c4473
Jo Egner <vechema@gmail.com>
2018-05-04 07:43:34 -0500

    Reformat code and factor out camera setup for resize

--- core/src/com/jegner/factory/rancher/MyGdxGame.java
+++ core/src/com/jegner/factory/rancher/MyGdxGame.java
@@ -1,6 +1,6 @@
 package com.jegner.factory.rancher;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
@@ -10,78 +10,80 @@ import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
-public class MyGdxGame extends ApplicationAdapter {
-	// Map
-	private TiledMap map;
-	private AssetManager manager;
+public class MyGdxGame extends Game {
+    // Map
+    private TiledMap map;
+    private AssetManager manager;
 
-	// Map properties
-	private int tileWidth, tileHeight,
-			mapWidthInTiles, mapHeightInTiles,
-			mapWidthInPixels, mapHeightInPixels;
+    // Map properties
+    private int tileWidth, tileHeight,
+            mapWidthInTiles, mapHeightInTiles,
+            mapWidthInPixels, mapHeightInPixels;
 
-	//Display size;
-	private int displayWidth;
-	private int displayHeight;
+    //Display size;
+    private int displayWidth;
+    private int displayHeight;
 
-	// Camera and render
-	private OrthographicCamera camera;
-	private OrthogonalTiledMapRenderer renderer;
+    // Camera and render
+    private OrthographicCamera camera;
+    private OrthogonalTiledMapRenderer renderer;
 
-	@Override
-	public void create () {
-		// Map loading
-		manager = new AssetManager();
-		manager.setLoader(TiledMap.class, new TmxMapLoader());
-		manager.load("dirt/dirtMap.tmx", TiledMap.class);
-		manager.finishLoading();
-		map = manager.get("dirt/dirtMap.tmx", TiledMap.class);
+    @Override
+    public void create() {
+        String dirtMapFileName = "dirt/dirtMap.tmx";
 
-		// Read properties
-		MapProperties properties = map.getProperties();
-		tileWidth         = properties.get("tilewidth", Integer.class);
-		tileHeight        = properties.get("tileheight", Integer.class);
-		mapWidthInTiles   = properties.get("width", Integer.class);
-		mapHeightInTiles  = properties.get("height", Integer.class);
-		mapWidthInPixels  = mapWidthInTiles  * tileWidth;
-		mapHeightInPixels = mapHeightInTiles * tileHeight;
+        // Map loading
+        manager = new AssetManager();
+        manager.setLoader(TiledMap.class, new TmxMapLoader());
+        manager.load(dirtMapFileName, TiledMap.class);
+        manager.finishLoading();
+        map = manager.get(dirtMapFileName, TiledMap.class);
 
-		// Set up the camera
-		displayWidth = Gdx.graphics.getWidth();
-		displayHeight = Gdx.graphics.getHeight();
+        // Read properties
+        MapProperties properties = map.getProperties();
+        tileWidth = properties.get("tilewidth", Integer.class);
+        tileHeight = properties.get("tileheight", Integer.class);
+        mapWidthInTiles = properties.get("width", Integer.class);
+        mapHeightInTiles = properties.get("height", Integer.class);
+        mapWidthInPixels = mapWidthInTiles * tileWidth;
+        mapHeightInPixels = mapHeightInTiles * tileHeight;
 
-		camera = new OrthographicCamera(displayWidth, displayHeight);
-		camera.zoom = 0.3f;
-		camera.position.x = mapWidthInPixels * .5f;
-		camera.position.y = mapHeightInPixels * .5f;
+        // Set up the camera
+        camera = new OrthographicCamera();
+        camera.zoom = 0.3f;
+        setUpCamera();
 
-		// Instantiation of the render for the map object
-		renderer = new OrthogonalTiledMapRenderer(map);
-	}
+        // Instantiation of the render for the map object
+        renderer = new OrthogonalTiledMapRenderer(map);
+    }
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		// Update the camera and render
-		camera.update();
-		renderer.setView(camera);
-		renderer.render();
-	}
+        // Update the camera and render
+        camera.update();
+        renderer.setView(camera);
+        renderer.render();
+    }
 
-	@Override
+    @Override
     public void resize(int width, int height) {
-	    displayWidth = Gdx.graphics.getWidth();
+        setUpCamera();
+    }
+
+    private void setUpCamera() {
+        displayWidth = Gdx.graphics.getWidth();
         displayHeight = Gdx.graphics.getHeight();
-	    camera.setToOrtho(true, displayWidth, displayHeight);
+        camera.setToOrtho(false, displayWidth, displayHeight);
         camera.position.x = mapWidthInPixels * .5f;
         camera.position.y = mapHeightInPixels * .5f;
     }
 
-	@Override
-	public void dispose () {
-		// Free resources
-		manager.dispose();
-	}
+    @Override
+    public void dispose() {
+        // Free resources
+        manager.dispose();
+    }
 }
--- desktop/src/com/jegner/factory/rancher/desktop/DesktopLauncher.java
+++ desktop/src/com/jegner/factory/rancher/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.jegner.factory.rancher.MyGdxGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1200;
+		config.height = 600;
 		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
