44a488d19d93a8f2dcd4b0cbf3d6c2e62ae9ba93
Jo Egner <vechema@gmail.com>
2018-07-02 21:20:25 -0500

    Add automatic texture packing

--- core/src/com/jegner/factory/rancher/resource/GameResourceNames.java
+++ core/src/com/jegner/factory/rancher/resource/GameResourceNames.java
@@ -4,7 +4,7 @@ public class GameResourceNames {
     public static final String dirtMapFileName = "dirt/dirtMap.tmx";
 
     // Human resources
-    public static final String humanAtlasFileName = "Player/human.atlas";
+    public static final String humanAtlasFileName = "Output/human.atlas";
     public static final String humanWalkDown = "human_walk_down";
     public static final String humanWalkUp = "human_walk_up";
     public static final String humanWalkRight = "human_walk_right";
--- desktop/src/com/jegner/factory/rancher/desktop/DesktopLauncher.java
+++ desktop/src/com/jegner/factory/rancher/desktop/DesktopLauncher.java
@@ -2,6 +2,7 @@ package com.jegner.factory.rancher.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 import com.jegner.factory.rancher.FactoryRancherGame;
 
 public class DesktopLauncher {
@@ -9,6 +10,7 @@ public class DesktopLauncher {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 800;
 		config.height = 600;
+        TexturePacker.process("./Player","./Output","human");
 		new LwjglApplication(new FactoryRancherGame(), config);
 	}
 }
